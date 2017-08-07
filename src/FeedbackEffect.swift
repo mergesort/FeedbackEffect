a87a07dd6bb45457f502c34016920e6def1d968f
Joe Fabisevich <mergesort@users.noreply.github.com>
2017-08-06 20:13:08 -0400

    Adding documentation

--- src/FeedbackEffect.swift
+++ src/FeedbackEffect.swift
@@ -2,6 +2,12 @@ import AudioToolbox
 import AVFoundation
 import UIKit
 
+/// An enum describing the different kinds of haptic feedback the taptic engine is capable of producing.
+/// For use in `FeedbackEffect`, to give the user feedback when it's desired.
+///
+/// - impact: Used for calling `UIImpactFeedbackGenerator().impactOccurred()`.
+/// - selection: Used for calling `UISelectionFeedbackGenerator().selectionChanged()`.
+/// - notification: Used for calling `UINotificationFeedbackGenerator().notificationOccurred(notificationType)`.
 public enum HapticFeedback {
 
     case impact
@@ -26,12 +32,14 @@ private extension HapticFeedback {
     }
 }
 
+/// A protocol for encapsulating the different ways that sound can be created, for use in `FeedbackEffect`.
 public protocol SoundEmitting {
 
     func makeSound()
 }
 
-public enum TextTone: Int {
+/// A set of text message sound effects that iOS provides through the `AudioServicesPlaySystemSound` subsystem.
+public enum MessageTone: Int {
 
     case note = 1375
     case aurora = 1366
@@ -75,13 +83,14 @@ public enum TextTone: Int {
     case update = 1036
 }
 
-extension TextTone: SoundEmitting {
+extension MessageTone: SoundEmitting {
 
     public func makeSound() {
         AudioServicesPlaySystemSound(UInt32(self.rawValue))
     }
 }
 
+/// A set of sound effects that iOS provides through the `AudioServicesPlaySystemSound` subsystem.
 public enum SoundEffect: Int {
 
     case tap = 1104
@@ -94,6 +103,8 @@ extension SoundEffect: SoundEmitting {
     }
 }
 
+/// A set of vibration patterns that iOS uses through the `AudioServicesPlaySystemSound` subsystem
+/// to generate vibrations on devices that do not have a taptic engine.
 public enum VibrationEffect: Int {
 
     case selectionVibration = 1519
@@ -118,10 +129,17 @@ extension URL: SoundEmitting {
     }
 }
 
+/// A type which encapsulates sound and haptic feedback. This is useful for providing feedback on user
+/// interaction, or when an app would like to signal a noteworthy event.
 public struct FeedbackEffect {
 
     static let player = AVQueuePlayer()
 
+    /// A function that allows a user to provide feedback to a user.
+    ///
+    /// - Parameters:
+    ///   - sound: A `SoundEmitting` type to play when feedback occurs.
+    ///   - feedback: A `HapticFeedback` type to play when feedback occurs.
     public static func play(sound: SoundEmitting?, feedback: HapticFeedback? = nil) {
         if let sound = sound {
             do {
